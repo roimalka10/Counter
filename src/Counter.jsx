c41fe706be7504f00c16cf416c2f32dde23de6a6
roi malka <roimalka10@gmail.com>
2021-05-07 21:30:36 +0300

    Initial commit

---
+++ src/Counter.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Button } from "@material-ui/core";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p> The count is: {count} </p>
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setCount(count + 1)}
+      >
+        Increment
+      </Button>
+      <br />
+      <br />
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => setCount(count - 1)}
+      >
+        Decrement
+      </Button>
+    </div>
+  );
+};
+
+export default Counter;
